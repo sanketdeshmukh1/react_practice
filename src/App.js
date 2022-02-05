c0539dcf8585e443c3c27d0e592d298b5fab14e4
Sanket Deshmukh <deshmukhsanket19@gmail.com>
2022-02-05 20:18:10 +0530

    navbar componant added
    navbar componant added in app.js and  componanr.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Componants/Navbar';
+
+function App() {
+  return (
+      
+      <>
+      <Navbar/>
+      </>
+
+      );
+}
+
+export default App;
