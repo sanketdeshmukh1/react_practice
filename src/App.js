100e692facabf64df183b437f218becdb5069d5b
Sanket Deshmukh <deshmukhsanket19@gmail.com>
2022-02-07 16:21:30 +0530

    Added routing
    Routing is done with the help of react routing,
    Changes in file app.js, about.js,navbar.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Componants/Navbar';
 import Textarea from './Componants/Textarea';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './Componants/About';
 
 function App() {
   return (
       <>
-      <Navbar title="Mynavbar" />
-      <div className="container my-3">
-    <Textarea heading="Enter the text in below box"/> 
-    </div>
-        </>
+      <Router>
+       <Navbar title="Mynavbar" />
+       <div className="container my-3">       
+       <Switch>
+            <Route path="/tex">
+            <Textarea heading="Enter the text in below box"/> 
+            </Route>
+            <Route path="/about">
+              <About heading="About page Enter the text in below box"/>
+            </Route>
+      </Switch>
+            </div>   
+          </Router>
+         </>
 
       );
 }
---
+++ src/Componants/About.js
@@ -0,0 +1,68 @@
+import React,{useState} from 'react';
+
+
+export default function About(props) {
+
+  const handleClickUp= ()=>{
+let uptext=text.toUpperCase();
+setText(uptext);
+  }
+
+  const handleChange= (event)=>{
+    //console.log("Handle change");
+      setText(event.target.value);
+}
+    
+
+const toggleColor= ()=>{
+    if(style1.color=='Black'){
+
+        setStyle( {
+            color:'White',
+            backgroundColor:'black'
+            }
+           );
+           setbtntog("Enable White Mode")
+        
+    }
+    else{
+
+        setStyle( {
+            color:'Black',
+            backgroundColor:'White'
+            }
+           );
+
+           setbtntog("Enable Dark Mode")
+     
+        
+    }
+    
+      }
+    
+      
+  const [text, setText]=useState("About page add text here");
+  
+  const [btntog, setbtntog]=useState("About page  Enable Dark Mode");
+  
+  const[style1,setStyle]=useState(
+    {
+    color:'black',
+    backgroundColor:'White'
+    }
+    );
+
+
+    return (
+      <div>
+  <div class="mb-3">
+    <label for="textarea1" style={style1} className="form-label">{props.heading}</label>
+    <textarea className="form-control" style={style1} onChange={handleChange} value={text} id="textarea1" rows="8"></textarea>
+  </div>
+<button className="textbutoon"  onClick={handleClickUp}>Convert to uppercase</button>
+<button className="darkbutoon"  onClick={toggleColor}>{btntog}</button>
+
+
+  </div>
+    );
+}
